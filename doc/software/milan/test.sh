d495f14fb1af1c7bd6f9fb275aaec2a3a877a1d3
mikiMilan <milanpredojevic1@gmail.com>
2020-09-09 20:51:16 +0200

    results for w_type 1 greedy 2

--- doc/software/milan/test.sh
+++ doc/software/milan/test.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 for i in $(seq 8 2 200)
 do
-   ./greedy.out -f preprocessing/small/min-$i-1_D1.txt -t 10 -greedy 3 -l result/result_D1_small_greedy3_w-type1.txt
+   ./greedy.out -f preprocessing/small/min-$i-1_D1.txt -t 10 -greedy 2 -l result/result_D1_small_greedy2_w-type1.txt
 done
