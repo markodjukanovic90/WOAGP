b5aab69672dfe0e00b3e6d73e2d48e62139b17ff
djukanovic <djukanovic.marko90@gmail.com>
2020-09-10 17:23:42 +0200

    CP i Cplex

--- doc/software/ReadMe.txt
+++ doc/software/ReadMe.txt
@@ -1,10 +1,10 @@
 Parametri programa: 
 
--i : sraza ka instanci 
+-f : sraza ka instanci 
 -t: vrijeme izvrsavana CPLEX-a 
 -alg: tip alg. koji pozivamo
       0: CPLEX
-      1: Greedy
+      1: CP
 
 Primjer poziva:
-./cplex_woagp -i rand.txt -t 100 -alg 1 
+./cplex_woagp -f milan/preprocessing/small/min-10-1_D1.txt -t 1000000 -alg 1 -w_type 0
--- doc/software/cplex_woagp.cpp
+++ doc/software/cplex_woagp.cpp
@@ -27,6 +27,19 @@
 
 // the following "include" is necessary for the correct working/compilation of CPLEX. You should adapt this path to your installation of CPLEX
 #include "/home/djukanovic/Desktop/projects/LCAPS_software/cplex-12.5/include/ilcplex/ilocplex.h"
+typedef pair<float, float> Point_2;
+/** pomocne strukture **/
+
+vector<float> Surface; // for a vertex indexed by i, we return the surface of V(i)
+vector<float> Cost; // at index i --> w_i
+vector<vector<float>>Intersection; // Intersection[i][j] = Volume of V(i) intersected by V(j)
+vector<set<Point_2>> S; // i-th position of S is a set of all points that are visible from vertex i
+set<Point_2> D_P;
+vector<Point_2> Vertices; // vector of vertices
+#define INFEASIBLE 1000000
+int cardinalityD; // |D(P)|
+float wTotal = 0.0; // total weight
+int w_type = 0; // ti ptezine koju pozivamo; 0: tezina proporcionalna velicini vidljivosti svakog vrha; 1: --; 2: --
 
 using namespace std; 
 
@@ -36,7 +49,7 @@ ILOSTLBEGIN
 time_t t;
 
 // vector for keeping the names of the input files
-vector<string> inputFile;
+string inputFile;
 int sigma = 0;
 // time limit for CPLEX (can be changed at run time via the -t comand line parameter)
 double time_limit = 900.0;
@@ -44,6 +57,8 @@ string outdir="/home/djukanovic/Desktop/projects/rflcsp/MIP-RFLCS-2d/CP-instance
 int m = 0; // number of sets
 int n = 0; // cardnality of X, i.e., |X|
 int alg = 0; // koji algoritam pozivamo 0: CPLEX (WSC); 1: Greedy (WSC)
+
+// end pomocne strukture
 
 inline int stoi(string &s) {
 
@@ -55,16 +70,148 @@ inline double stof(string &s) {
        return atof(s.c_str());
 }
 
+float dist(int i, int j)
+{
+     return sqrt( pow( Vertices[i].first - Vertices[j].first, 2) + pow( Vertices[ i ].second - Vertices[ j ].second, 2) );
+
+}
+
+void read_from_file(std:: string path)
+{
+     // TODO: read from file
+     std::ifstream infile(path);
+     std::string line;
+     std::getline(infile, line);
+     int n = stoi(line); // number of vertices
+
+    int temp = 0;
+
+    while(temp<n){
+        std::getline(infile, line);
+
+        std::string delim1 = ":";
+         auto start = 0U;
+         auto end = line.find(delim1);
+
+         //izdvajanje koordinata tjemena (vrha) poligona
+         std:: string  vrh =  line.substr(start, end - start);
+
+         std::string delim2 = ",";
+
+         auto start1 = 0U;
+         auto end1 = vrh.find(delim2);
+
+         std:: string  vrh1 =  vrh.substr(start1+1, end1 - start1-1);
+         std:: string  vrh2 =  vrh.substr(end1+1, vrh.length()-end1-2);
+
+
+         Point_2 vertex;
+         vertex.first = stof(vrh1);
+         vertex.second = stof(vrh2);
+         D_P.insert(vertex);
+         Vertices.push_back(vertex);
+
+        //izdvjanje skupa tacaka koje se vide iz tog vrha
+
+        std:: string ostatak = line.substr(end+1, line.length() - end);
+
+        std::string delimiter = "\t";
+
+        size_t pos = 0;
+        std::string token;
+        set<Point_2> vertexSet;
+
+        while ((pos = ostatak.find(delimiter)) != std::string::npos) {
+            token = ostatak.substr(0, pos);
+            std::cout << token << std::endl;
+
+            //obrada svakog tokena da bi se dobile koordinate tacaka
+
+
+         std::string delim3 = ",";
+
+         auto start3 = 0U;
+         auto end3 = vrh.find(delim3);
+
+         std:: string  x =  token.substr(start3+1, end3 - start3-1);
+         std:: string  y =  token.substr(end3+1, token.length()-end3-2);
+
+         Point_2 tacka;
+         tacka.first = stof(x);
+         tacka.second = stof(y); 
+         D_P.insert(tacka);
+
+        vertexSet.insert(tacka);
+        ostatak.erase(0, pos + delimiter.length());
+    }
+    S.push_back(vertexSet);
+    temp++;
+   }
+    //izdvajanje povrsina
+    temp = 0;
+    while(temp<n){
+        std::getline(infile, line);
+
+        std::string delim1 = ":";
+         auto start = 0U;
+         auto end = line.find(delim1);
+
+        std:: string ostatak = line.substr(end+1, line.length() - end);
+
+        float povrsina = stof(ostatak);
+         Surface.push_back(povrsina);
+        temp++;
+
+    }
+
+
+   //izdvjanje presjeka
+
+   temp = 1;
+   vector<float> tempVect;
+    while(std::getline(infile, line))
+    {
+
+         std::string delim1 = ":";
+         auto start = 0U;
+         auto end = line.find(delim1);
+
+          std:: string  vrhovi =  line.substr(start, end - start);
+          std:: string ostatak = line.substr(end+1, line.length() - end);
+
+
+
+        float povrsinaPresjeka = stof(ostatak);
+
+
+        if(temp<n){
+            tempVect.push_back(povrsinaPresjeka);
+            temp++;
+        }
+
+        if(temp==n){
+            Intersection.push_back(tempVect);
+            tempVect.clear();
+            temp=1;
+            n--;
+        }
+    }
+    cout << S.size() << " n: " << n << " Surface " << Surface.size() << endl;
+}
+
+
 void read_parameters(int argc, char **argv) {
 
     int iarg = 1;
 
     while (iarg < argc) {
-        if (strcmp(argv[iarg],"-i")==0) inputFile.push_back(argv[++iarg]);
+        if (strcmp(argv[iarg],"-f")==0) inputFile = argv[++iarg];
         else if (strcmp(argv[iarg],"-t")==0) time_limit = atof(argv[++iarg]);
         else if (strcmp(argv[iarg],"-alg")==0) alg = atof(argv[++iarg]);
+        else if (strcmp(argv[iarg],"-w_type")==0) w_type = atof(argv[++iarg]);
         iarg++;
-    }
+    } 
+     cout << "w_type " << w_type << endl;
 }
 
 // write during the CPLEX call
@@ -88,141 +235,40 @@ ILOMIPINFOCALLBACK6(loggingCallback,
     lastIncumbent = nv;
 }
 
-int f(vector<set<int>>& C)
-{
-       std::set<int> d;
-       for(set<int> di : C){
-     
-           for (int dii: di){
-               //cout << "dii" << dii << endl;
-               d.insert(dii);
-           }
-        
-       }
-       //cout <<"D" << d.size() << endl; 
-       //for(int diii: d)
-       //  cout << diii << "\t" <<endl;
+void run_cplex(vector<set<Point_2>>& S, set<Point_2>& D_P, int n){
 
-       return (int)d.size();
-}
-
-int f_minus(vector<set<int>>& C, set<int>& ss ) // f(C u {s}) - f(S)
-{
-    int count = 0; // ako i \in ss nije niti u jednom od skupova u S, onda count++;
-    if(C.empty())
-       return ss.size();
-
-    for(int i: ss) 
-    {
-        bool presented = false;
-        for(set<int>& di : C){
-            if(di.count(i) > 0){
-               presented = true;
-               break; 
-            }
-        }
-        if(presented) 
-           count++;
-    }
-    return ss.size() - count; 
-}
-
-float greedy_criterion(vector<int>& Cost, vector<set<int>>& S, int i, vector<set<int>>& C) // take s_i from S
-{     
-    set<int> s = S[i];
-    int f_m = f_minus(C, s);  //cout << "f_m: " << f_m << endl;
-    if(f_m == 0)
-       return 100000;
-
-    float val = ((float)Cost[i]) / (f_minus(C, s));     //cout << "val: " << val << endl; 
-    return val;
-}
-
-bool findA(vector<int>& s, int a)
-{
-
-     for(int si : s)
-         if(si == a)
-            return true;
-
-      return false;  
-}
-
-int min_greedy(vector<int>& Cost, vector<set<int>>& S, vector<set<int>>& C, vector<int>& indeks)
-{         //cout << "min_greedy" << endl;
-          float g_m = 10000000; int dodaj;
-          for(int i = 0; i < S.size(); ++i)
-          {  // cout << "i " << i << endl;
-              float g_mi = greedy_criterion(Cost, S, i, C); //cout << "gmi: " << g_mi << endl;
-              if(g_mi < g_m and g_mi != 100000 and !findA(indeks, i)) 
-              { 
-                 dodaj = i;
-                 g_m = g_mi;   
-              }
-          }//cout << "dodati....." << dodaj << endl;
-          return dodaj;
-}
-
-
-float greedy_procedure(vector<set<int>>& S, vector<int>& Cost)
-{
-     vector<set<int>> C; vector<int> indeks;
-     int f_S = f(S); //cout << "f_S: " << f_S << endl;
-     int f_C = 0;
-     while(f_C != f_S) 
-     {
-           int index_set = min_greedy( Cost, S, C, indeks);  
-           
-           if(!findA(indeks, index_set)){ //jos nije dodan
-               C.push_back(S[index_set]);cout << "dodaj ----> " << index_set << endl;
-              indeks.push_back(index_set);
-           } 
-           f_C = f(C); 
-           cout << "f_C=" << f_C << " |C|=" << C.size() << endl;
-     }
-     float greedy_val = 0;
-     for(int ix: indeks){
-         greedy_val += Cost[ ix ];
-     }
-     return greedy_val; 
-     //return C.size();
-}
-
-
-void run_cplex(vector<int>& C, vector<set<int>>& S){
-
-  IloEnv env;
+  IloEnv env;  cout << "run_cplex " << endl;
   env.setOut(env.getNullStream());
   try
   {
-   // define the LCIS problem model...
    IloModel model(env);
    IloObjective obj = IloMinimize(env);
    // defining the set of binary variables Z
-   vector<IloNumVar> Z; cout << "S.size() " << S.size() << endl;
-   for(int i = 0; i < S.size(); ++i){
-
+   vector<IloNumVar> Z; cout << "S.size() ----> " << S.size() << n << endl;
+   for(int i = 0; i < n; ++i){
+       cout << "cost i " << Cost[i] << endl;
        IloNumVar myIntVar(env, 0, 1, ILOINT);
        Z.push_back(myIntVar); // x_i  \in {0, 1}
-       obj.setLinearCoef(Z[i], C[i]); // sum_i c_i x_i
+       obj.setLinearCoef(Z[i], Cost[i]); // sum_i c_i x_i
    }
-
-   cout << "#Vars: " << Z.size() << " n=" << n << " " << S.size() << endl;
    // constraints 
-   for(int i = 0; i < n; ++i) 
+   cout << "#Vars: " << Z.size() << endl;
+   // constraints 
+   int index = 0;
+   for(Point_2 p : D_P) 
    {
        IloExpr expr_i(env); bool add = false;
-       for(int j = 0; j < S.size(); ++j)   
+       for(int j = 0; j < n; ++j)   
        {
-           if( S[j].count(i+1) ){ // item i se nalazi u skupu j
+           if( S[j].count( p ) ){ // item i se nalazi u skupu j
                expr_i += Z[j];
                add = true;
            }
        }
        if(add)
           model.add(expr_i >= 1);      
-       
-   }
+       index++ ;
+   }  
 
    //std::cout << "#Constraints: "<< constraints_num << std::endl;
    //solve the modelIloLinearNumExpr objective = cplex.linearNumExpr();
@@ -249,6 +295,7 @@ void run_cplex(vector<int>& C, vector<set<int>>& S){
           cout << "CPLEX finds feasible solution" << endl;
 
        double lastVal = double(cplex.getObjValue());
+       cout << "\n CPLEX sol: " << lastVal << endl;
        // print the objective point
        cout << "Sets in the solution: {" <<endl;
        bool first = true;  
@@ -262,7 +309,6 @@ void run_cplex(vector<int>& C, vector<set<int>>& S){
 
        }
        cout << "}" << endl;
-       cout << "\n CPLEX sol: " << lastVal << endl;
        //myfileOut << "value: " << lastVal << endl;
    }
    else cout << "Nema rjesenja" << endl;
@@ -275,7 +321,7 @@ env.end();
 
 
 
-int run_cp(vector<int>& C, vector<set<int>>& S){
+int run_cp(vector<set<Point_2>>& S, set<Point_2>& D_P, int n){
 
   IloEnv env; cout << "Run CP" << endl;
   env.setOut(env.getNullStream());
@@ -286,37 +332,39 @@ int run_cp(vector<int>& C, vector<set<int>>& S){
    IloObjective obj = IloMinimize(env);
    // defining the set of binary variables Z
    vector<IloNumVar> Z; cout << "S.size() " << S.size() << endl;
-   for(int i = 0; i < S.size(); ++i){
-
+   for(int i = 0; i < n; ++i){
+       //cout << "w_i: " << Cost[i] << endl;
        IloNumVar myIntVar(env, 0, 1, ILOINT);
        Z.push_back(myIntVar); // x_i  \in {0, 1}
-       obj.setLinearCoef(Z[i], C[i]); // sum_i c_i x_i
+       obj.setLinearCoef(Z[i], Cost[i]); // sum_i c_i x_i
    }
-
    cout << "#Vars: " << Z.size() << " n=" << n << " " << S.size() << endl;
    // constraints 
-   for(int i = 0; i < n; ++i) 
+   int index = 0;
+   for(Point_2 p : D_P) 
    {
        IloExpr expr_i(env); bool add = false;
-       for(int j = 0; j < S.size(); ++j)   
+       for(int j = 0; j < n; ++j)   
        {
-           if( S[j].count(i+1) ){ // item i se nalazi u skupu j
+           if( S[j].count( p )){ // item i se nalazi u skupu j
+               //cout << "j: " << j << endl;
                expr_i += Z[j];
                add = true;
            }
        }
        if(add)
           model.add(expr_i >= 1);      
-       
+        index++;
    }  
    model.add(obj);
    IloCP cp(model);
-   cp.setParameter(IloCP::FailLimit, 30000);
+   cp.setParameter(IloCP::FailLimit, 3000000);
    // the following two parameters should always be set in the way as shown
 
    if (cp.solve())
    {
        double lastVal = double(cp.getObjValue());
+       cout << "Cp sol: " << lastVal << endl;
        // print the objective point
        cout << "Sets in the solution: {" <<endl;
        bool first = true;  
@@ -330,9 +378,7 @@ int run_cp(vector<int>& C, vector<set<int>>& S){
 
        }
        cout << "}" << endl;
-   return lastVal;
    }
-   return -1;
  }catch(IloException& e) {
         cerr  << " ERROR: " << e << endl;
 }
@@ -340,7 +386,39 @@ env.end();
 }
 
 
-	
+void fill_cost()
+{
+// ----------------------- dodjela tezina (proporcionalno broju pokrivenih tacaka iz D(P) ------------------------------------
+    cout << "fill cost " << w_type << endl;
+    switch(w_type){
+ 
+          case 0: { // tezina proporcionalna sa velicinom skupa S[i]
+                  for(auto& X: S){
+                      cardinalityD += X.size();
+                  }
+                  for (size_t i = 0; i < n; i++){
+                      float w_i = n * n * ( ((float) S[i].size()) / cardinalityD );
+                      wTotal += w_i;cout << "w_i " << w_i << endl;
+                      Cost.push_back(w_i); 
+                  }; break;}
+          case 1: {     
+                  float dist0  = (dist(0, n-1) + dist(0,1) ) / 2; 
+                  Cost.push_back(dist0); wTotal += dist0;
+                   for(int i = 1; i < n; ++i){ 
+                      float w_i = ( (0.0 + dist(i-1, i) + dist(i, i+1) ) / 2 );
+                      Cost.push_back( w_i );
+                      wTotal += w_i;
+                      cout << "w_i= " << Cost[i] << endl;
+                   }
+                     break;}
+          default: {
+                    for(size_t i = 0; i < n; i++) // non-weighted version of the problem
+                       Cost.push_back(1);
+                   }
+   }
+
+
+}
 /**********
 Main function
 **********/
@@ -358,82 +436,34 @@ int main(int argc, char **argv ) {
         
         cout << " Reading parameters " << endl;
         read_parameters(argc,argv);
-    
         //setting the output format for doubles to 2 decimals after the comma
         std::cout << std::setprecision(2) << std::fixed;
         Timer timer;
         // generisanje stream-a
         double cur_time = timer.elapsed_time(Timer::VIRTUAL);
         // opening the corresponding input file and reading the problem data
-
-	vector< set<int> > S;
-	vector <int> C; // randomly select cost of each set S_i
-         
-	ifstream input;
-	input.open(inputFile[0].c_str());
-        //input >> m >> n; cout << "m=" << m << " n=" << n << endl;
-        //std::string str1;
-        //std::getline(input, str1);  
-        std::string str;
-        string content; int x;
-        int line = 0;
-        while(std::getline(input, content)) {
-              //cout << content << ' ';
-              if(line >= 1) // prva linija je vec procitana
-              {   
-                 std::stringstream   linestream(content);
-                 set<int> S_i;
-                 while(linestream >> x){
-                    //cout << x << endl;
-                    S_i.insert(x);
-                 }
-                 if(line % 2 == 0){ // svaka druga linija ocitava jedan skup:
-                     S.push_back(S_i);
-                     S_i.clear();
-                     int k = 1 + rand() % 10;
-                     C.push_back(k);    // random cijena od [0,99]
-                     //cout << k << endl;
-                 }
-              }
-              else{
-                  std::stringstream   linestream(content);
-                  linestream >> m >> n; cout <<"n---> " << n << endl;
-              }
-              line++;
-        }
-	
-        input.close();
-        cout << "End of input " << inputFile[0].c_str() << ".out" << endl;
-        cout << "|S|=" << S.size() <<endl; 
-
-        for(int i = 0; i < S.size(); ++i){
-            cout << "read S_" << i << endl;
-            for(set<int>::iterator it = S[i].begin(); it != S[i].end(); ++it){
-                cout << (*it) << endl;
-            }
-         }
-         // write the results in the output...
-         std::stringstream filenameOut;
-         filenameOut << outdir << inputFile[0].c_str() << ".out";
-         if(alg == 0) 
-         { 
+        read_parameters(argc, argv);
+        read_from_file(inputFile); n = S.size();// fill Cost, Intersection and Surface
+        read_from_file(inputFile); cout << "s" << S.size() << "n: " << n << endl;
+        fill_cost(); cout << "cost--->" << Cost.size() << " n: " << n << endl;
+        if(alg == 0) 
+        { 
             cout << "Run CPLEX..." << endl;
-            run_cplex(C, S); //, myfileOut);
+            run_cplex(S, D_P, n); //, myfileOut);
             double end_time = timer.elapsed_time(Timer::VIRTUAL);
             cout << "CPLEX time:" << (end_time - cur_time ) <<"\n";
             // greedy procedure:
-         }else{
+        }else{
         
-            Timer timer_greedy;
+            Timer timer_cp;
             // generisanje stream-a
-            double cur_time_greedy = timer_greedy.elapsed_time(Timer::VIRTUAL);
-            float greedy_sol = run_cp(C, S); 
-            cout <<"sol: " << greedy_sol << endl;
-            double end_time_greedy = timer_greedy.elapsed_time(Timer::VIRTUAL);
-            cout <<"time: " << (end_time_greedy - cur_time_greedy) << endl;
+            double cur_time_cp = timer_cp.elapsed_time(Timer::VIRTUAL);
+            run_cp(S, D_P, n);; 
+            double end_time_cp = timer_cp.elapsed_time(Timer::VIRTUAL);
+            cout <<"time: " << (end_time_cp - cur_time_cp) << endl;
             //myfileOut << "time: " << (end_time - cur_time ) <<"\n";*/
             //myfileOut.close();
-         }
+        }
 }
 
 
